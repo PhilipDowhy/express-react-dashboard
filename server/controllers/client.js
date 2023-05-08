83c516125cdd79b3ddb2e7470a2674905bc081fe
Philip <pdowhy@comcast.net>
2023-05-08 17:02:33 -0400

    adding geography route

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 
 export const getProducts = async (req, res) => {
@@ -34,7 +35,7 @@ export const getCustomers = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
- 
+
 export const getTransactions = async (req, res) => {
   try {
     // sort should look like this: { "field": "userId", "sort": "desc"}
@@ -60,7 +61,7 @@ export const getTransactions = async (req, res) => {
       .sort(sortFormatted)
       .skip(page * pageSize)
       .limit(pageSize);
-    
+
     const total = await Transaction.countDocuments({
       name: { $regex: search, $options: "i" },
     });
@@ -73,3 +74,29 @@ export const getTransactions = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const getGeography = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
+
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count }
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
